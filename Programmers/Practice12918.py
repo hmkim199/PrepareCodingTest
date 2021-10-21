779b5684ccd49156b8d2216fa10e469e4d6ac95c
hmkim199 <hmkim199@gmail.com>
2021-10-21 17:14:38 +0900

    solve 12918

---
+++ Programmers/Practice12918.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12918
+# 문자열 다루기 기본
+
+# 예전 코드
+def solution(s):
+    answer = False
+    if len(s) == 4 or len(s) == 6:
+        answer = s.isdigit()
+    return answer
+
+# 지금 코드 -> 파이썬 삼항연산자 활용
+def solution(s):
+    answer = s.isdigit() if len(s) == 4 or len(s) == 6 else False
+
+    return answer
