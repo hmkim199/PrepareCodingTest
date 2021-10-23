3add424628cf25c9198223788f4e38f4adc1a5a6
hmkim199 <hmkim199@gmail.com>
2021-10-23 17:32:09 +0900

    solve 12915

---
+++ Programmers/Practice12915.py
@@ -0,0 +1,18 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12915
+# 문자열 내 마음대로 정렬하기
+
+
+# 예전 코드
+def solution(strings, n):
+    answer = strings
+    answer.sort()
+    answer.sort(key = lambda x : x[n], reverse = False)
+    return answer
+
+
+# 지금 코드
+def solution(strings, n):
+    answer = strings
+    answer.sort()
+    answer.sort(key=lambda x: x[n])
+    return answer
