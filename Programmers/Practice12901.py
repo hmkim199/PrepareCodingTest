296f212bec087a0b935878df50a2704af3edd9f2
hmkim199 <hmkim199@gmail.com>
2021-10-31 04:03:58 +0900

    feat: solve 12901

--- Programmers/Practice12901.py
+++ Programmers/Practice12901.py
@@ -11,3 +11,11 @@ def solution(a, b):
     days += b
     days = days % 7
     return week[days]
+
+
+# 현재 코드
+import datetime
+def solution(a, b):
+    days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    answer = days[datetime.date(2016, a, b).weekday()]
+    return answer
