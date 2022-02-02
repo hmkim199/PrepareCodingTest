1c046a3b1b76761559272e421fe0348a3d2cd0ed
hmkim199 <hmkim199@gmail.com>
2022-02-03 01:18:11 +0900

    feat: solve 2164

---
+++ Baekjoon/Practice2164.py
@@ -0,0 +1,35 @@
+# https://www.acmicpc.net/problem/2164
+# 카드2
+
+# 시간 초과
+# N = int(input())
+# nums = [i+1 for i in range(N)]
+
+# while len(nums) > 1:
+#     nums.pop(0)
+#     nums.append(nums.pop(0))
+# print(nums[0])
+
+# 시간 초과
+# N = int(input())
+# nums = [i+1 for i in range(N)]
+
+# while len(nums) > 1:
+#     nums = nums[2:]+[nums[1]]
+# print(nums[0])
+
+
+N = int(input())
+nums = [i+1 for i in range(N)]
+idx = 0
+
+while True:
+    if len(nums) - idx > 1:
+        idx += 1
+        nums.append(nums[idx])
+        idx += 1
+    else:
+        break
+
+print(nums[idx])
+
