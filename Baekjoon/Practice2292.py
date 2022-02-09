4302fd9480f6795d23abaf6ca25636e6550fdfb4
hmkim199 <hmkim199@gmail.com>
2022-02-09 17:41:45 +0900

    feat: solve 2292

--- Baekjoon/Practice2292.py
+++ Baekjoon/Practice2292.py
@@ -1,6 +1,7 @@
 # https://www.acmicpc.net/problem/2292
 # 벌집
 
+# 저번 풀이
 N = int(input())
 
 now = 1
@@ -9,4 +10,27 @@ while now < N:
     now += 6*min_room
     min_room += 1
 print(min_room)
+
+# 이번 풀이
+N = int(input())
+room = 1
+count = 1
+shape = 6
+while room < N:
+    if room + shape * count <= N:
+        room += shape * count
+    else:
+        room = N
+    count += 1
+
+print(count)
+
+
+
+
+
+
+
+
+
 
