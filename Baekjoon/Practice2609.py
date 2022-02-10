1f1578c0e168f20605ea23e29a02e78c55ac8a01
hmkim199 <hmkim199@gmail.com>
2022-02-10 17:51:14 +0900

    style: add comment

--- Baekjoon/Practice2609.py
+++ Baekjoon/Practice2609.py
@@ -1,6 +1,8 @@
 # https://www.acmicpc.net/problem/2609
 # 최대공약수와 최소공배수
 
+# 참고 링크 : https://velog.io/@junyp1/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C-%ED%98%B8%EC%A0%9C%EB%B2%95 유클리드 호제법
+
 a, b = map(int, input().split())
 a, b = max(a, b), min(a, b)
 
