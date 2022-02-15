c8c39d31f8a1f0d4d4d08dacf4be2ea57e1c739b
hmkim199 <hmkim199@gmail.com>
2022-02-16 00:39:04 +0900

    refactor: modify 2798

--- Baekjoon/Practice2798.py
+++ Baekjoon/Practice2798.py
@@ -2,15 +2,15 @@
 # 블랙잭
 
 N, M = map(int, input().split())
-nums = list(map(int, input().split()))
+cards = list(map(int, input().split()))
 
-total = []
-length = len(nums)
+total = 0
+length = len(cards)
 for i in range(length-2):
     for j in range(i+1, length-1):
         for k in range(j+1, length):
-            temp = nums[i] + nums[j] + nums[k]
-            if temp <= M:
-                total.append(temp)
+            temp = cards[i] + cards[j] + cards[k]
+            if total < temp <= M:
+                total = temp
 
-print(max(total))
+print(total)
