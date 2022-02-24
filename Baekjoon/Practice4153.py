ac43e6efa006964ee2eadaf635b0d41c0d2a0661
hmkim199 <hmkim199@gmail.com>
2022-02-25 01:10:06 +0900

    feat: solve 4153

--- Baekjoon/Practice4153.py
+++ Baekjoon/Practice4153.py
@@ -1,15 +1,47 @@
 # https://www.acmicpc.net/problem/4153
 # 직각삼각형
 
-isPythagoras = []
+# 예전 풀이
 
+# isPythagoras = []
+
+# while True:
+#     triangle = sorted(list(map(int, input().split())))
+#     if triangle[0] == 0 and triangle[1] == 0 and triangle[2] == 0:
+#         break
+#     if triangle[0] ** 2 + triangle[1] ** 2 == triangle[2] ** 2:
+#         isPythagoras.append("right")
+#     else:
+#         isPythagoras.append("wrong")
+
+# print("\n".join(isPythagoras))
+
+
+# 새 풀이
+res=[]
 while True:
-    triangle = sorted(list(map(int, input().split())))
-    if triangle[0] == 0 and triangle[1] == 0 and triangle[2] == 0:
+    t = sorted(list(map(int, input().split())))
+    if sum(t) == 0:
         break
-    if triangle[0] ** 2 + triangle[1] ** 2 == triangle[2] ** 2:
-        isPythagoras.append("right")
+    elif t[0]**2 + t[1]**2 == t[2]**2:
+        res.append("right")
     else:
-        isPythagoras.append("wrong")
+        res.append("wrong")
 
-print("\n".join(isPythagoras))
+print(*res, sep="\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
