27da2a1df6c2622f0012b1f0963f7b8fa1f1d812
hmkim199 <hmkim199@gmail.com>
2022-03-18 13:07:21 +0900

    feat: solve 11050

--- Baekjoon/Practice11050.py
+++ Baekjoon/Practice11050.py
@@ -1,12 +1,26 @@
 # https://www.acmicpc.net/problem/11050
 # 이항 계수 1
 
+# N, K = map(int, input().split())
+
+# result = 1
+# for i in range(K):
+#     result *= (N-i)
+# for i in range(K, 0, -1):
+#     result /= i
+
+# print(int(result))
+
+
+# int로 감싸지 않으면 틀림.
+# 지금 풀이
+
 N, K = map(int, input().split())
 
-result = 1
-for i in range(K):
-    result *= (N-i)
-for i in range(K, 0, -1):
-    result /= i
+res = 1
+for i in range(N-K):
+    res *= (N-i)
+for i in range(N-K, 0, -1):
+    res /= i
 
-print(int(result))
+print(int(res))
