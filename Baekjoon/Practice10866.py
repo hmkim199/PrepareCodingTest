83cc8e07ce987cf60ebc83d96024cd493494119c
hmkim199 <hmkim199@gmail.com>
2022-03-18 09:50:43 +0900

    feat: solve10866

--- Baekjoon/Practice10866.py
+++ Baekjoon/Practice10866.py
@@ -1,9 +1,28 @@
 # https://www.acmicpc.net/problem/10866
 # 덱
 
+from collections import deque
 import sys
 
-N = int(sys.stdin.readline().strip().split())
+N = int(input())
+d = deque()
 
 for _ in range(N):
     op = sys.stdin.readline().strip().split()
+    if op[0] == "push_back":
+        d.append(int(op[1]))
+    elif op[0] == "push_front":
+        d.appendleft(int(op[1]))
+    elif op[0] == "front":
+        print(d[0]) if d else print(-1)
+    elif op[0] == "back":
+        print(d[-1]) if d else print(-1)
+    elif op[0] == "pop_front":
+        print(d.popleft()) if d else print(-1)
+    elif op[0] == "pop_back":
+        print(d.pop()) if d else print(-1)
+    elif op[0] == "size":
+        print(len(d))
+    elif op[0] == "empty":
+        print(0) if d else print(1)
+
