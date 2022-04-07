bfa7716e4a60d2dbbc914e68aebfb20ebf789903
hmkim199 <hmkim199@gmail.com>
2022-04-07 15:20:18 +0900

    feat: solve 10869

--- Baekjoon/Practice10818.py
+++ Baekjoon/Practice10818.py
@@ -1,7 +1,19 @@
 # https://www.acmicpc.net/problem/10818
 # 최소, 최대
 
-N = int(input())
-nums = list(map(int, input().split()))
+# N = int(input())
+# nums = list(map(int, input().split()))
 
-print(min(nums), max(nums))
+# print(min(nums), max(nums))
+
+
+# https://www.acmicpc.net/problem/10869
+# 사칙연산 
+
+A, B = map(int, input().split())
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
