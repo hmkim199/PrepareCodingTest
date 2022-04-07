ce3ea9e4939c04b5baff40c6e87d8148423fcb5c
hmkim199 <hmkim199@gmail.com>
2022-04-07 15:48:22 +0900

    feat: solve 12921

--- Baekjoon/Practice10818.py
+++ Baekjoon/Practice10818.py
@@ -42,3 +42,38 @@ def solution(n):
 def solution(n):
     answer = '수박'*(n//2)
     return answer if n%2 == 0 else answer+'수'
+
+
+# https://programmers.co.kr/learn/courses/30/lessons/12921
+# 소수 찾기
+
+# 예전 풀이
+def solution(n):
+    answer = 0
+    isPrime = [True for _ in range(n+1)]
+    isPrime[0], isPrime[1] = False, False
+    
+    for i in range(len(isPrime)):
+        if isPrime[i] == True:
+            answer += 1
+            for j in range(2, len(isPrime)):
+                if i*j > len(isPrime)-1:
+                    break
+                isPrime[i*j] = False
+
+    
+    return answer
+
+# 이번 풀이
+# 단순 2중 for문 돌면 시간 초과
+def solution(n):
+    answer = 0
+    is_prime = [True for _ in range(n+1)]
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, n+1):
+        if is_prime[i] == True:
+            answer += 1
+            for j in range(2, n//i+1):
+                is_prime[i*j] = False
+
+    return answer
