3794d3d0cb17baec1e784beff95f432762664de6
hmkim199 <hmkim199@gmail.com>
2022-04-07 15:17:59 +0900

    feat: solve 10818

--- Baekjoon/Practice2579.py
+++ Baekjoon/Practice2579.py
@@ -13,6 +13,7 @@ if num > 1:
     dp[1] = stairs[0]+stairs[1]
 if num > 2:
     dp[2] = max(stairs[0]+stairs[2], stairs[1]+stairs[2])
+    
 for i in range(3, num):
     dp[i] = max(dp[i-3]+stairs[i-1]+stairs[i], dp[i-2]+stairs[i])
 
