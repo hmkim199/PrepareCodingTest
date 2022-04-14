09daf94a4c0fdb30e680ba1d381b5626c6dadbf0
hmkim199 <hmkim199@gmail.com>
2022-04-14 15:38:27 +0900

    feat: solve 10950

--- Baekjoon/Practice10950.py
+++ Baekjoon/Practice10950.py
@@ -2,7 +2,18 @@
 # A+B - 3
 
 
-T = int(input())
-for _ in range(T):
-    A, B = map(int, input().split())
-    print(A+B)
+# T = int(input())
+# for _ in range(T):
+#     A, B = map(int, input().split())
+#     print(A+B)
+
+
+# https://www.acmicpc.net/problem/10951
+# A+B - 4
+
+while True:
+    try:
+        A, B = map(int, input().split())
+        print(A+B)
+    except:
+        break
