ead27a607bc6700e0ac8ba008e58059f84dc4334
hmkim199 <hmkim199@gmail.com>
2022-04-28 15:34:01 +0900

    feat: solve 10998 11654 11720

---
+++ Baekjoon/Practice10998.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/10998
+# A×B
+
+A, B = map(int, input().split())
+print(A*B)
+
+
+# https://www.acmicpc.net/problem/11654
+# 아스키 코드
+
+s = input()
+print(ord(s))
+
+
+# https://www.acmicpc.net/problem/11720
+# 숫자의 합
+
+N = int(input())
+nums = map(int, list(input()))
+print(sum(nums))
