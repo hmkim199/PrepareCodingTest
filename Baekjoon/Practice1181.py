e7597354ef3403dbc7a030adb195d495d4a36c2a
hmkim199 <hmkim199@gmail.com>
2022-05-03 17:13:50 +0900

    feat: solve 1181

--- Baekjoon/Practice1181.py
+++ Baekjoon/Practice1181.py
@@ -1,13 +1,23 @@
 # https://www.acmicpc.net/problem/1181
 # 단어 정렬
 
+# N = int(input())
+
+# words = []
+# for i in range(N):
+#     words.append(input().strip())
+
+# words = list(set(words))
+# words.sort()
+# words.sort(key=len)
+# print(*words, sep="\n")
+
+
 N = int(input())
+words = set()
+for _ in range(N):
+    words.add(input())
 
-words = []
-for i in range(N):
-    words.append(input().strip())
+words = sorted(words, key=lambda x: (len(x), x))
 
-words = list(set(words))
-words.sort()
-words.sort(key=len)
 print(*words, sep="\n")
