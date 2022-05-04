72c8a5c2a5698b707117e5651224fca336af1912
hmkim199 <hmkim199@gmail.com>
2022-05-04 23:09:32 +0900

    feat: solve dfs practice

--- ThisIsCodingTest/1.py
+++ ThisIsCodingTest/1.py
@@ -1,4 +1,6 @@
 # 음료수 얼려먹기
+# ==================================================================
+# bfs
 
 from collections import deque
 
@@ -33,3 +35,37 @@ for i in range(N):
             count += 1
 
 print(count)
+
+
+# =====================================================================
+# dfs
+
+N, M = map(int, input().split()) # 세로, 가로
+ice = []
+visited = [[False for _ in range(M)] for _ in range(N)]
+
+for _ in range(N):
+    ice.append(input().strip())
+
+def dfs(x, y):
+    global visited
+    if x < 0 or x >= N or y < 0 or y >= M:
+        return False
+    
+    if ice[x][y] == "0" and not visited[x][y]:
+        visited[x][y] = True
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+count = 0
+for i in range(N):
+    for j in range(M):
+        if dfs(i, j):
+            count += 1
+
+print(count)
+
