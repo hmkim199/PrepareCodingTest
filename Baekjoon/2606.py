22555d59236b2027e42d149735ea8209e83434b5
hmkim199 <hmkim199@gmail.com>
2022-05-11 23:38:58 +0900

    feat: solve 2606

--- Baekjoon/2606.py
+++ Baekjoon/2606.py
@@ -1,6 +1,31 @@
 # https://www.acmicpc.net/problem/2606
 # 바이러스 
 
+# 풀이 1. 풀이 2보다 시간, 공간 효율 높음
+n = int(input())
+m = int(input())
+v = 1
+matrix = [[0 for i in range(n+1)] for j in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    matrix[a][b] = 1
+    matrix[b][a] = 1
+
+def bfs(v):
+    queue = [v]
+    visited = [v]
+    while len(queue) > 0:
+        now = queue.pop(0)
+        for i in range(len(matrix[now])):
+            if matrix[now][i] == 1 and i not in visited:
+                queue.append(i)
+                visited.append(i)
+    return len(visited)-1
+
+print(bfs(v))
+
+# 풀이 2
 from collections import deque
 
 N = int(input())
