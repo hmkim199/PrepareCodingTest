d2c1ff3960652865b3e1692f74b72c0637d76d2e
hmkim199 <hmkim199@gmail.com>
2022-05-17 15:28:12 +0900

    feat: solve 1920

--- Programmers/week16.py
+++ Programmers/week16.py
@@ -33,33 +33,68 @@
 # else:
 #     print(*operation, sep="\n")
     
-    
-from collections import deque
+
+# ===========================================  
+# from collections import deque
 
 
-n = int(input())
-seq = deque()
-for _ in range(n):
-    seq.append(int(input()))
+# n = int(input())
+# seq = deque()
+# for _ in range(n):
+#     seq.append(int(input()))
 
-stack = deque()
-operation = []
+# stack = deque()
+# operation = []
 
-for i in range(1, n+1):
-    if i == seq[0]:
-        operation.append('+')
-        operation.append('-')
-        seq.popleft()
-        while stack and stack[-1] == seq[0]:
-            operation.append('-')
-            seq.popleft()
-            stack.pop()
-    else:
-        operation.append('+')
-        stack.append(i)
+# for i in range(1, n+1):
+#     if i == seq[0]:
+#         operation.append('+')
+#         operation.append('-')
+#         seq.popleft()
+#         while stack and stack[-1] == seq[0]:
+#             operation.append('-')
+#             seq.popleft()
+#             stack.pop()
+#     else:
+#         operation.append('+')
+#         stack.append(i)
 
-if stack:
-    print("NO")
+# if stack:
+#     print("NO")
 
-else:
-    print(*operation, sep="\n")
+# else:
+#     print(*operation, sep="\n")
+
+# https://www.acmicpc.net/problem/1920
+# 수 찾기
+
+
+# N = int(input())
+# A = sorted(map(int, input().split()))
+# M = int(input())
+# B = list(map(int, input().split()))
+
+# for i in B:
+#     start = 0
+#     end = N-1
+#     middle = (start+end)//2
+
+#     found = False
+#     while start <= end:
+#         middle = (start+end)//2
+#         if A[middle] == i:
+#             found = True
+#             break
+#         elif A[middle] > i:
+#             end = middle-1
+#         else:
+#             start = middle+1
+#     if found:
+#         print(1)
+#     else:
+#         print(0)
+
+# =========================================
+# https://www.acmicpc.net/problem/1929
+# 소수 구하기
+
