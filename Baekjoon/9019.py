d47dba509575b95f6a3e7ec9a36e2a99ac60ee95
hmkim199 <hmkim199@gmail.com>
2022-05-22 22:23:55 +0900

    feat: solve 9019

--- Baekjoon/9019.py
+++ Baekjoon/9019.py
@@ -1,7 +1,7 @@
 # https://www.acmicpc.net/problem/9019
 # DSLR
 
-# 시간 초과!!!
+# pypy로 통과. python 은 시간초과
 from collections import deque
 
 
@@ -26,18 +26,16 @@ for _ in range(T):
         dic = {0:"D", 1:"S", 2:"L", 3:"R"}
         DSLR.append(temp * 2 % 10000)
         DSLR.append(temp - 1 if temp != 0 else 9999)
-        temp = str(temp)
-        if len(temp) < 4:
-            temp = "0" * (4 - len(temp)) + temp
-        DSLR.append(int(temp[1:]+temp[0]))
-        DSLR.append(int(temp[3]+temp[0:3]))
+        DSLR.append(temp%1000*10+temp//1000)
+        DSLR.append(temp%10*1000+temp//10)
         for i in range(4):
             if DSLR[i] == B:
                 result.append(operations+dic[i])
                 found = True
                 break
-            queue.append([DSLR[i], operations+dic[i]])
-            visited.add(DSLR[i])
+            if DSLR[i] not in visited:
+                queue.append([DSLR[i], operations+dic[i]])
+                visited.add(DSLR[i])
 
 print(*result, sep="\n")
         
