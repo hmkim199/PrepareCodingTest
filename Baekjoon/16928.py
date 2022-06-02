9a601746362f535df471f24c8fd7e1a84104741f
hmkim199 <hmkim199@gmail.com>
2022-06-02 23:21:52 +0900

    feat: solve 16928

--- Baekjoon/16928.py
+++ Baekjoon/16928.py
@@ -18,30 +18,36 @@ for _ in range(M):
     u, v = map(int, sys.stdin.readline().rstrip().split())
     board[u] = v
 
-# 첫 번째 사다리 만나기 전까진 일반 주사위.
-# 9퍼에서 틀렸습니다
-
+# 현재 위치에서 주사위 1~6이 나왔을 때 각각을 사다리인 경우, 뱀인 경우에 큐에 추가. 일반 칸인 경우에는 일반 칸 중 가장 큰 수만 큐에 추가.
 def bfs():
     queue = deque()
     queue.append([1, 0])
     while queue:
         now, cnt = queue.popleft()
-        temp = now
+        local_max = now # 일반 칸 중 가장 큰 수
+
+        # 현 위치에서 주사위 숫자 1~6의 경우의 수
         for i in range(1, 7):
             if not visited[now+i]:
                 if now+i < dest:
                     visited[now+i] = True
+                    # 사다리인 경우
                     if board[now+i] > now+i:
                         visited[board[now+i]] = True
                         queue.append([board[now+i], cnt+1])
+                    
+                    # 일반 칸
                     elif board[now+i] == now+i:
-                        temp = now+i
+                        local_max = now+i
+                    
+                    # 뱀 타는게 이득인 경우.
+                    else:
+                        visited[board[now+i]] = True
+                        queue.append([board[now+i], cnt+1])
                 elif now+i == dest:
                     return cnt+1
-                # 뱀 타는게 이득인 경우. 이거 생각해서 고치기
-                # else:
-                #     queue.append([board[now+i], cnt+1])
-        queue.append([temp, cnt+1])
+                
+        queue.append([local_max, cnt+1])
     return -1
 
 print(bfs())
