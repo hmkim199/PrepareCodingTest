3c47525419cdf6b8482df5567412d0daab2fa2b2
hmkim199 <hmkim199@gmail.com>
2022-06-13 15:30:01 +0900

    feat: solve 1780

--- Baekjoon/1780.py
+++ Baekjoon/1780.py
@@ -7,32 +7,6 @@ for _ in range(N):
     paper.append(list(map(int, input().split())))
 
 cnt = {-1: 0, 0: 0, 1: 0}
-# found = [[False for _ in range(N)] for _ in range(N)]
-
-# def is_all_same(x, y, n):
-#     global cnt
-
-#     for i in range(n):
-#         for j in range(n):
-#             if paper[x+i][y+j] != paper[x][y]:
-#                 return False
-#     cnt[paper[x][y]] += 1
-#     return True
-
-
-# def count_paper(N):
-#     if N == 1:
-#         return True
-
-#     n = N//3
-#     for i in range(0, N, n):
-#         for j in range(0, N, n):
-#             if not is_all_same(i, j, n):
-#                 count_paper(n)
-
-# if not is_all_same(0, 0, N):
-#     count_paper(N)
-
 
 def count_paper(x, y, n):
     global cnt
