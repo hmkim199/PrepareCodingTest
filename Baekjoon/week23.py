f5da61f0d1b67b798709c845f99eaa990b87d621
hmkim199 <hmkim199@gmail.com>
2022-07-15 17:36:03 +0900

    week 23, working on week 25

--- Baekjoon/week23.py
+++ Baekjoon/week23.py
@@ -151,18 +151,91 @@ print(*bfs_result)
 # https://programmers.co.kr/learn/courses/30/lessons/12945
 # 피보나치 수
 
+def solution(n):
+    answer = [0, 1, 1]
+    for i in range(3, n + 1) :
+        answer.append(answer[i-1] + answer[i-2])
+    
+    
+    return (answer[n]) % 1234567
+
 
 # https://programmers.co.kr/learn/courses/30/lessons/12941
 # 최솟값 만들기
+
+def solution(A, B) :
+    answer = 0
+    A.sort()
+    B.sort(reverse=True)
+
+    for i in range(len(A)) :
+        answer += A[i] * B[i]
+
+    return answer
 
 
 # https://programmers.co.kr/learn/courses/30/lessons/12939
 # 최댓값과 최솟값
 
+def solution(s):
+    answer = sorted(map(int, s.split(" ")))
+    answer = str(answer[0]) + " " + str(answer[-1])
+    return answer
+
 
 # https://programmers.co.kr/learn/courses/30/lessons/92341
 # 주차 요금 계산
 
+import math
+from collections import defaultdict
+
+def solution(fees, records):
+    answer = []
+
+    dict = {}
+    total = defaultdict(int)
+    # 주차한 시간
+    for record in records :
+        time, number, state = record.split() # 시간, 차량 번호, 상태(In or Out)
+        hour, minutes = time.split(":")
+        time = int(hour) * 60 + int(minutes)
+        if number in dict : # 이미 입차되어 있다면
+            total[number] += time - dict[number]
+            del dict[number]
+        else : # 입차할 경우
+            dict[number] = time
+
+    # 출차를 안 한 경우
+    max_time = (23 * 60) + 59
+    for num, t in dict.items():
+        total[num] += max_time - t
+
+    # 요금 계산
+    basic_minutes, basic_fee, split_minutes, split_fee = fees
+    for num, t in total.items() :
+        cost = basic_fee
+        if t > basic_minutes : # 추가 요금 발생
+            cost += math.ceil((t - basic_minutes) / split_minutes) * split_fee # 올림 처리
+        answer.append((num, cost))
+
+    # 차량 번호 오름차순
+    answer.sort()
+    return [value[1] for value in answer]
 
 # https://programmers.co.kr/learn/courses/30/lessons/12936
 # 줄 서는 방법
+
+import math
+def solution(n, k):
+    arr = [i for i in range(1, n+1)]
+    answer = []
+    while n > 0:
+        n -= 1
+        fac = math.factorial(n)
+        div = k // fac
+        mod = k % fac
+        if mod == 0:
+            div -= 1
+        answer.append(arr.pop(div))
+        k = mod
+    return answer
