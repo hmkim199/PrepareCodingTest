40c3c2e14559ae8636e2588075c80288edf89d40
hmkim199 <hmkim199@gmail.com>
2022-07-24 23:52:51 +0900

    feat: solve 1043

--- Baekjoon/1043.py
+++ Baekjoon/1043.py
@@ -1,25 +1,35 @@
 # https://www.acmicpc.net/problem/1043
 # 거짓말
 
+from collections import deque
+
+
 N, M = map(int, input().split()) # 사람의 수, 파티의 수
 t_num = list(map(int, input().split()))
 t_cnt = t_num[0] # 진실을 아는 사람의 수
-t_num = set(t_num[1:])
+t_num = deque(t_num[1:]) # 진실을 아는 사람 번호
 
-
-cnt = 0
+parties = []
+lie_party = [True for _ in range(M)] # 거짓말 하는 파티인지
 for _ in range(M):
     p_num = list(map(int, input().split()))
-    p_cnt = p_num[0]
-    p_num = set(p_num[1:])
+    p_cnt = p_num[0] # 파티 인원
+    p_num = set(p_num[1:]) # 파티 참여 사람 번호
+    parties.append(p_num) # 파티 정보 저장
 
-    can_lie = True
-    for t in t_num:
-        if t in p_num:
-            print(p_num)
-            can_lie = False
-            break
-    if can_lie:
+while t_num:
+    t = t_num.popleft() # 진실을 아는 사람
+    for i in range(len(parties)):
+        if lie_party[i] and t in parties[i]:
+            lie_party[i] = False
+            
+            if len(parties[i]) > 1:
+                parties[i].remove(t)
+                for new_known in parties[i]:
+                    t_num.append(new_known)
+
+cnt = 0
+for p in lie_party:
+    if p:
         cnt += 1
-
 print(cnt)
