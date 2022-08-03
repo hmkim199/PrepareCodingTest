e0c4bfbb92809a0d2545580e3499a6543253ada1
hmkim199 <hmkim199@gmail.com>
2022-08-03 18:57:19 +0900

    feat: solve 12913 - dp

--- Programmers/12913.py
+++ Programmers/12913.py
@@ -1,26 +1,20 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12913
 # 땅따먹기
 
-# 시간초과
-from collections import deque
-
+# 검색해서 풂.
+# 참고: https://unie2.tistory.com/1050
 
 def solution(land):
-    maximum = 0
-    queue = deque()
-    for i in range(4):
-        queue.append((land[0][i], (0, i)))
-
-    while queue:
-        total, pos = queue.popleft()
-        if pos[0] < len(land)-1:            
-            # 다음으로
-            for i in range(4):
-                if i != pos[1]:
-                    queue.append((total+land[pos[0]+1][i], (pos[0]+1, i)))
-        else:
-            maximum = max(maximum, total)
-
-    return maximum
+    dp = [[0 for _ in range(len(land[0]))] for _ in range(len(land))]
+    for i in range(len(land)):
+        for j in range(len(land[0])):
+            if i == 0:
+                dp[i][j] = land[i][j]
+            else:
+                for k in range(len(land[0])):
+                    if j == k:
+                        continue
+                    dp[i][j] = max(dp[i][j], dp[i-1][k]+land[i][j])
+    return max(dp[-1])
 
 print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
