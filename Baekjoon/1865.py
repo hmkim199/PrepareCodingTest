721346a9d977bb147a291a63b14dba886e5a57b7
hmkim199 <hmkim199@gmail.com>
2022-08-19 11:45:40 +0900

    feat: solve 1865!

--- Baekjoon/1865.py
+++ Baekjoon/1865.py
@@ -58,7 +58,6 @@
 # 음의 순환이 있을 때 최단 경로를 다익스트라로 찾을 수 없다! 따라서 벨만 포드 알고리즘 사용한다.
 # 벨만포드 가장 잘 설명된 유튜브 링크 : https://www.youtube.com/watch?v=0NrlN88D9Fs
 
-# 틀렸습니다.
 import sys
 
 input = sys.stdin.readline
@@ -73,30 +72,37 @@ def bellman_ford(N, edges, start):
         for s, e, t in edges:
             if dist[e] > dist[s] + t:
                 dist[e] = dist[s] + t
-                
-    return dist[start]
+
+    return dist
+
+
+# 시간 역행 가능하면 True 반환
+def time_reverse(dist, edges):
+    for s, e, t in edges:
+        if dist[e] > dist[s] + t:
+            return True
+    return False
+
 
 for _ in range(TC):
     N, M, W = map(int, input().split())
-    # graph = [[] for _ in range(N+1)]
     edges = []
 
     for _ in range(M):
         S, E, T = map(int, input().split())
-        # graph[S].append((E, T))
-        # graph[E].append((S, T))
         edges.append((S, E, T))
         edges.append((E, S, T))
     
     for _ in range(W):
         S, E, T = map(int, input().split())
         T = -T if T > 0 else T
-        # graph[S].append((E, T))
         edges.append((S, E, T))
     
-    res = bellman_ford(N, edges, 1)
-    if res >= 0:
-        print("NO")
-    else:
+    dist = bellman_ford(N, edges, 1)
+    res = time_reverse(dist, edges)
+
+    if res:
         print("YES")
+    else:
+        print("NO")
 
