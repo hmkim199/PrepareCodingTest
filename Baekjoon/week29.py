eec941b5468eff79c1552e62ed1cc3ac849f6c44
hmkim199 <hmkim199@gmail.com>
2022-08-26 11:53:58 +0900

    feat: solve 70129

--- Baekjoon/week29.py
+++ Baekjoon/week29.py
@@ -250,3 +250,18 @@ def solution(n):
 print(solution(5)) # 2
 print(solution(6)) # 2
 print(solution(5000)) # 5
+
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/70129
+# 이진 변환 반복하기
+
+def solution(s):
+    answer = [0, 0] # 이진 변환 횟수, 제거한 0의 개수
+    
+    while s != "1":
+        zero = s.count('0')
+        answer[1] += zero
+        answer[0] += 1
+        s = str(bin(len(s) - zero))[2:]
+        
+    return answer
