6b35a967155acd7f4081524ac6905b0adde5d6ca
hmkim199 <hmkim199@gmail.com>
2022-08-30 01:36:20 +0900

    feat: working on solve 13460

--- Baekjoon/13460.py
+++ Baekjoon/13460.py
@@ -1,9 +1,7 @@
 # https://www.acmicpc.net/problem/13460
 # 구슬 탈출 2
 
-
-# 음.. 일단 실제 board를 바꾸면 안될것같음. 뭔가 접근법은 비슷한거같으면서..... 조금 더 생각해보기로!!!!!!!!
-
+# 86퍼에서 틀렸습니다
 from collections import deque
 
 
@@ -26,39 +24,27 @@ for i in range(N):
         elif s[j] == "O":
             hole = i, j
 
-visited = [[False for _ in range(M)] for _ in range(N)]
-visited[red[0]][red[1]] = True 
+visited = set()
+visited.add((red, blue))
 
-def go_straight(target, dx, dy, color):
+def go_straight(target, dx, dy, red, blue, visited = None):
     global board
-    global visited
-    # global red
-    # global blue
-    # global N
-    # global M
 
-    # is_moved = False
     new_x = target[0] + dx
     new_y = target[1] + dy
     
-    while board[new_x][new_y] == "." and (new_x, new_y) not in (red, blue): # 0 <= new_x < N and 0 <= new_y < M and 
-        # board[new_x][new_y] = color
-        # if color == "B":
-        #     blue = new_x, new_y
-        # else:
-        #     red = new_x, new_y
-        visited[new_x][new_y] = True        
-        # board[target[0]][target[1]] = "."
-        # is_moved = True
+    while board[new_x][new_y] == "." and (new_x, new_y) not in (red, blue):  
         target = new_x, new_y
         new_x = target[0] + dx
         new_y = target[1] + dy
+    
+    if (new_x, new_y) == hole:
+        target = new_x, new_y
         
-    # return is_moved, new_x, new_y
     return target
 
 def marble(R, B, O):
-    min_move = -1
+    min_move = 11
     q = deque([(R, B, 0)]) # 0은 move
 
     while q:
@@ -67,41 +53,34 @@ def marble(R, B, O):
             continue
 
         for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            # is_moved = False
-            # temp_moved, R_x, R_y = go_straight(red, dx, dy, "R")
-            R = go_straight(red, dx, dy, "R")
-            # if temp_moved:
-            #     is_moved = True
-            
-            # if board[R_x][R_y] == "B":
+            R = go_straight(red, dx, dy, red, blue, visited)
             if (R[0]+dx, R[1]+dy) == blue:
-                # temp_moved, B_x, B_y = go_straight(blue, dx, dy, "B")
-                B = go_straight(blue, dx, dy, "B")
-                # if board[B_x][B_y] == "O":
+                B = go_straight(blue, dx, dy, R, blue)
                 if (B[0]+dx, B[1]+dy) == hole:
                     continue
-                # R = R_x - dx, R_y - dy
-                # temp_moved, R_x, R_y = go_straight(red, dx, dy, "R")
-                R = go_straight(R, dx, dy, "R")
-
-                # if temp_moved:
-                #     is_moved = True
+                if R == hole:
+                    return move + 1
+                R = go_straight(R, dx, dy, R, B, visited)
             
             else:
-                # temp_moved, B_x, B_y = go_straight(blue, dx, dy, "B")
-                B = go_straight(blue, dx, dy, "B")
+                B = go_straight(blue, dx, dy, R, blue)
+                if R == hole:
+                    if R != B:
+                        # min_move = min(min_move, move+1)
+                        return move + 1
+                    continue
+                elif B == hole:
+                    continue
 
-
-            # if is_moved:
-            if red != R:
-                new_move = move + 1
-                # new_R = R_x - dx, R_y - dy
-                # new_B = B_x - dx, B_y - dy
-                q.append((R, B, new_move))
-
-            if board[R[0]+dx][R[1]+dy] == "O":
-                min_move = min(min_move, new_move)
+            if red != R or blue != B:
+                if (R, B) not in visited:
+                    visited.add((R, B))
+                    q.append((R, B, move+1))
         
-    return move
+    return min_move
 
-print(marble(red, blue, hole))
+count = marble(red, blue, hole)
+if count > 10:
+    print(-1)
+else:
+    print(count)
