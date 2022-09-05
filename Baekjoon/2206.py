c2e1400634e31ae7dc89debea8c11340aa720118
hmkim199 <hmkim199@gmail.com>
2022-09-05 17:00:06 +0900

    feat: solve 2206

--- Baekjoon/2206.py
+++ Baekjoon/2206.py
@@ -1,45 +1,85 @@
 # https://www.acmicpc.net/problem/2206
 # 벽 부수고 이동하기
 
-# 5퍼에서 시간초과
-
 from collections import deque
-from copy import deepcopy
+import sys
 
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 matrix = []
 
 for _ in range(N):
-    matrix.append(list(map(int, list(input()))))
+    matrix.append(list(map(int, list(input().strip()))))
 
 
 def bfs(matrix, M, N):
-    minimum = 10e9
-    visited = [[False for _ in range(M)] for _ in range(N)]
-    visited[0][0] = True
+    visited = [[[0, 0] for _ in range(M)] for _ in range(N)]
+    visited[0][0][0] = 1
     q = deque()
-    q.append([0, 0, 1, False, visited]) # x, y, 최단거리, 벽 부쉈는지
+    q.append([0, 0, 0]) # x, y, 벽 부쉈는지
     while q:
-        i, j, mini, is_crashed, visited = q.popleft()
+        i, j, k = q.popleft()
         if i == N-1 and j == M-1:
-            minimum = min(minimum, mini)
-            continue
+            return visited[i][j][k]
 
         for dx, dy in ((1, 0), (-1, 0), (0, -1), (0, 1)):
             x = i+dx
             y = j+dy
-            if 0 <= x < N and 0 <= y < M:
+            if 0 <= x < N and 0 <= y < M and not visited[x][y][k]:
                 if matrix[x][y] == 0:
-                    if not visited[x][y]:
-                        visited[x][y] = True
-                        q.append([x, y, mini+1, is_crashed, visited])
+                    visited[x][y][k] = visited[i][j][k] + 1
+                    q.append([x, y, k])
                 
-                else:
-                    if not is_crashed and not visited[x][y]:
-                        visited[x][y] = True
-                        q.append([x, y, mini+1, True, deepcopy(visited)])
+                elif not k:
+                    visited[x][y][1] = visited[i][j][k] + 1
+                    q.append([x, y, 1])
 
-    return minimum if minimum < 10e9 else -1
+    return -1
 
 print(bfs(matrix, M, N))
+
+
+# 한끝 차이인데 왜 틀렸을까
+# # https://www.acmicpc.net/problem/2206
+# # 벽 부수고 이동하기
+
+# from collections import deque
+# import sys
+
+# input = sys.stdin.readline
+
+# N, M = map(int, input().split())
+# matrix = []
+
+# for _ in range(N):
+#     matrix.append(list(map(int, list(input().strip()))))
+
+
+# def bfs(matrix, M, N):
+#     visited = [[[0, 0] for _ in range(M)] for _ in range(N)]
+#     visited[0][0][0] = True
+#     q = deque()
+#     q.append([0, 0, 0]) # x, y, 벽 부쉈는지
+#     while q:
+#         i, j, k = q.popleft()
+#         if i == N-1 and j == M-1:
+#             return visited[i][j][k]
+
+#         for dx, dy in ((1, 0), (-1, 0), (0, -1), (0, 1)):
+#             x = i+dx
+#             y = j+dy
+#             if 0 <= x < N and 0 <= y < M:
+#                 if matrix[x][y] == 0:
+#                     if not visited[x][y][k]:
+#                         visited[x][y][k] = visited[i][j][k] + 1
+#                         q.append([x, y, k])
+                
+#                 else:
+#                     if not k and not visited[x][y][k]:
+#                         visited[x][y][1] = visited[i][j][k] + 1
+#                         q.append([x, y, 1])
+
+#     return -1
+
+# print(bfs(matrix, M, N))
