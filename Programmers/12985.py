6ae0976a1df1b6ceb13c268c4f0b96cc870e64cf
hmkim199 <hmkim199@gmail.com>
2022-10-21 14:29:21 +0900

    feat: working on solve 12985

---
+++ Programmers/12985.py
@@ -0,0 +1,27 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12985
+# 예상 대진표
+
+# 시간 초과
+# def next_number(num):
+#     """
+#     대결 후 다음 라운드에 가지는 숫자 리턴.
+#     """
+#     if num % 2 == 0:
+#         return num // 2
+#     else:
+#         return (num+1) // 2
+
+# def solution(n,a,b):
+#     answer = 1
+#     new_a = next_number(a)
+#     new_b = next_number(b)
+    
+#     while True:
+#         new_a, new_b = min(new_a, new_b), max(new_a, new_b)
+#         if new_b % 2 == 0 and new_b - 1 == new_a:
+#             return answer + 1
+#         else:
+#             new_a = next_number(new_a)
+#             new_b = next_number(new_b)
+#             answer += 1
+#     return answer + 1
